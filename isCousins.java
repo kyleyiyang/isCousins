265c6256d40923d2fe1bea1be039712b730ef9b6
kyleyiyang <kyleyiyang@gmail.com>
2020-11-25 17:11:17 -0500

    Update isCousins.java

--- isCousins.java
+++ isCousins.java
@@ -16,23 +16,20 @@
 class Solution {
     public boolean isCousins(TreeNode root, int x, int y) {
         List<Integer> lst = new ArrayList<>();
-        search(root,x,y,0,0,0,lst);
-        System.out.println(lst);
-        return lst.get(0)==lst.get(1);
+        search(root,x,y,-1,lst);
+        //System.out.println(lst);
+        return lst.get(0)==lst.get(1)?(lst.get(0)!=0):false;
     }
-    public static boolean search(TreeNode r, int x, int y, int k, int kx, int ky,List<Integer> lst) {
+    public static void search(TreeNode r, int x, int y, int k, List<Integer> lst) {
         if (r!=null) {
             k++;
             if (r.left!=null && r.right!=null) {
-                if ((r.left.val==x && r.right.val==y) || (r.left.val==y && r.right.val==x)) return false;
-            }
-            if (k>0 && r.val==x) lst.add(k);
-            if (k>0 && r.val==y) lst.add(k);
-            System.out.println("k="+k+"; kx="+kx+"; ky="+ky);
-            if (kx>0 && ky>0 && kx==ky) return true;
-            search(r.left,x,y,k,kx,ky,lst);
-            search(r.right,x,y,k,kx,ky,lst);
+                if ((r.left.val==x && r.right.val==y) || (r.left.val==y && r.right.val==x)) {lst.add(0);lst.add(0);}
+            } 
+            if (r.val==x) lst.add(k);
+            if (r.val==y) lst.add(k);
+            search(r.left,x,y,k,lst);
+            search(r.right,x,y,k,lst);
         }
-        return false;
     }
 }
